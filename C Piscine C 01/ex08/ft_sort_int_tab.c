477a91568a71f12ee3ae94edd5aec9cd5e885d32
Jong-hwan Seo <jseo@c10r3s14.42seoul.kr>
2020-10-22 13:21:38 +0900

    norminette

--- C Piscine C 01/ex08/ft_sort_int_tab.c
+++ C Piscine C 01/ex08/ft_sort_int_tab.c
@@ -6,7 +6,7 @@
 /*   By: jseo <jseo@student.42seoul.kr>             +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/10/20 22:14:28 by jseo              #+#    #+#             */
-/*   Updated: 2020/10/20 22:20:17 by jseo             ###   ########.fr       */
+/*   Updated: 2020/10/22 13:21:12 by jseo             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,7 +21,7 @@ void		ft_sort_int_tab(int *tab, int size)
 	{
 		j_ind = i_ind;
 		while (++j_ind < size)
-		{	
+		{
 			if (*(tab + i_ind) > *(tab + j_ind))
 			{
 				temp = *(tab + i_ind);
